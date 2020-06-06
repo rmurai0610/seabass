8ee7e481f6f854b57f3a2a6dd488227cff705ec0
Riku Murai <rmurai0610@gmail.com>
2020-06-06 17:02:26 +0100

    Clean up cmake file

--- examples/1.hello_world/main.cc
+++ examples/1.hello_world/main.cc
@@ -6,9 +6,11 @@
 int main() {
     auto ctx = sb::Context(sb::colorscheme(sb::Theme::LIGHT));
     auto window = ctx.create_window("Hello World", {960, 640});
-    auto tum_loader = sb::loader::ICLTrajectory();
-    tum_loader.load("examples/1.hello_world/data/trajectory.txt");
+    // Load trajectory data
+    auto loader = sb::loader::ICLTrajectory();
+    loader.load("examples/1.hello_world/data/trajectory.txt");
 
+    // Set coordinate frame to neg_y right, z up
     auto camera =
         ctx.create_camera3d(45.f, {0.1, 100.f}, sb::AxisDirection::AxisNegY,
                             sb::AxisDirection::AxisZ);
@@ -17,9 +19,8 @@ int main() {
     widgets.add<sb::Widget::Coordinate>(3.f);
     widgets.add<sb::Widget::Grid>(-1.f, camera->up_vec());
 
-    // auto keyframes = widgets.add<sb::Widget::Keyframes>(true, 0.1f);
     auto trajectory = widgets.add<sb::Widget::CameraTrajectory>();
-    trajectory->set_poses(tum_loader.camera_poses_T_wc());
+    trajectory->set_poses(loader.camera_poses_T_wc());
     while (ctx.render()) {
         widgets.render();
     }
