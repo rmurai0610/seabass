f2f232664a3f95507f5da573412530ec35cb1d6e
Riku Murai <rmurai0610@gmail.com>
2021-01-05 17:43:04 +0900

    Change templated VBO to float VBO

--- include/seabass/base/vertex_buffer.h
+++ include/seabass/base/vertex_buffer.h
@@ -6,36 +6,28 @@
 #include <vector>
 namespace sb {
 
-class VertexBufferBase {
-public:
-    virtual void bind_to_vertex_arrray(std::string attribute_name,
-                                       GLuint attribute_index) const = 0;
-    virtual size_t count() const = 0;
-};
-
-template <class T>
-class VertexBuffer : public VertexBufferBase {
+class VertexBuffer  {
 private:
-    std::vector<T> data;
+    std::vector<float> data;
     int element_size;
     GLuint id;
     int num_per_vert;
 
 public:
-    VertexBuffer(std::vector<T> data, int num_per_vert)
+    VertexBuffer(const std::vector<float> &data, int num_per_vert)
         : data(data), num_per_vert(num_per_vert) {
-        int element_size = sizeof(T);
+        element_size = sizeof(float);
         glGenBuffers(1, &id);
         glBindBuffer(GL_ARRAY_BUFFER, id);
         glBufferData(GL_ARRAY_BUFFER, this->data.size() * element_size,
                      this->data.data(), GL_DYNAMIC_DRAW);
         glBindBuffer(GL_ARRAY_BUFFER, 0);
     }
-    const std::vector<T> read() const { return data; }
-    std::vector<T> read() { return data; }
-    size_t count() const override { return this->data.size() / num_per_vert; }
+    const std::vector<float> read() const { return data; }
+    std::vector<float> read() { return data; }
+    size_t count() const { return this->data.size() / num_per_vert; }
     void bind_to_vertex_arrray(std::string attribute_name,
-                               GLuint attribute_index) const override {
+                               GLuint attribute_index) const {
         glBindBuffer(GL_ARRAY_BUFFER, id);
         glVertexAttribPointer(attribute_index, num_per_vert, GL_FLOAT, GL_FALSE,
                               0, 0);
@@ -47,17 +39,17 @@ class VertexBufferAttribute {
 private:
     std::string attribute_name_;
     GLuint attribute_index_;
-    sb::VertexBufferBase *vbo_;
+    sb::VertexBuffer *vbo_;
 
 public:
     VertexBufferAttribute(std::string attribute_name, GLuint attribute_index,
-                          sb::VertexBufferBase *vbo)
+                          sb::VertexBuffer *vbo)
         : attribute_name_(attribute_name),
           attribute_index_(attribute_index),
           vbo_(vbo) {}
     const std::string &attribute_name() const { return attribute_name_; }
     GLuint attribute_index() const { return attribute_index_; }
-    const sb::VertexBufferBase *vbo() const { return vbo_; }
-    sb::VertexBufferBase *vbo() { return vbo_; }
+    const sb::VertexBuffer *vbo() const { return vbo_; }
+    sb::VertexBuffer *vbo() { return vbo_; }
 };
 };  // namespace sb
