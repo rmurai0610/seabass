57c77a7e99a18271f5e4d8b1db3a2f202dbe6be6
Riku Murai <rmurai0610@gmail.com>
2021-01-25 23:43:38 +0900

    Ignore cache

--- build.sh
+++ build.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 if [ -d "./imgui" ]; then
-  echo "Checking if there are latest version of Dear ImGui";
-  cd imgui;
-  git fetch;
-  git checkout `git tag | sort -V | grep -v "\-rc" | tail -1`;
-  if [ $? -ne 0  ]; then
-    exit 1;
-  fi
-  cd ..;
+  echo "skip...";
+  #echo "Checking if there are latest version of Dear ImGui";
+  #cd imgui;
+  #git fetch;
+  #git checkout `git tag | sort -V | grep -v "\-rc" | tail -1`;
+  #if [ $? -ne 0  ]; then
+    #exit 1;
+  #fi
+  #cd ..;
 else
   git clone https://github.com/ocornut/imgui.git
   cd imgui;
-  git checkout `git tag | sort -V | grep -v "\-rc" | tail -1`;
+  #git checkout `git tag | sort -V | grep -v "\-rc" | tail -1`;
+  git checkout v1.79
   cd ..;
 fi
 
